e85c8652eb55ba5eb832270525e7d0da0fd628ae
Jakub Zięba <jakubzieba7@gmail.com>
2023-02-17 12:37:54 +0100

    Add project files.

---
+++ Tydz4_cw2_FizzBuzz/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Tydz4_cw2_FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wprowadź dowolną liczbę całkowitą i zagrajmy w FizzBuzz!");
+
+            FizzBuzz newTry= new FizzBuzz();
+            newTry.GetNumber();
+        }
+    }
+}
