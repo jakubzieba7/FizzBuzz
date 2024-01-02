c2374ac2a3edb7687e5fe1df591423b57ec16a2b
Jakub Zięba <jakubzieba7@gmail.com>
2024-01-02 11:49:33 +0100

    Refactoring FizzBuzz and Program classes

--- Tydz4_cw2_FizzBuzz/FizzBuzz.cs
+++ Tydz4_cw2_FizzBuzz/FizzBuzz.cs
@@ -2,29 +2,7 @@
 {
     public class FizzBuzz
     {
-        public void GetNumber()
-        {
-            Console.WriteLine("Jeżeli chcesz zakończyć działanie aplikacji wprowadź 'T'");
-
-            var input = Console.ReadLine();
-
-            while (true)
-            {
-                if (input.ToUpper() == "T")
-                {
-                    Environment.Exit(0);
-                }
-                if (!int.TryParse(input, out int number))
-                {
-                    Console.WriteLine("Wprowadzona liczba nie jest liczbą całkowitą lub wykracza poza jej zakres. Podaj nową liczbę całkowitą:");
-                    input = Console.ReadLine();
-                    continue;
-                }
-                FizzBuzzMessage(number);
-            }
-        }
-
-        public void FizzBuzzMessage(int number)
+        public static string Message(int number)
         {
             if (number % 3 == 0 && number % 5 == 0)
                 Console.WriteLine("Sukces! Twój wynik to 'FizzBuzz'\nJeśli chcesz grac dalej wprowadź nową liczbę.");
@@ -34,8 +12,10 @@
                 Console.WriteLine("Twój wynik to 'Buzz'\nJeśli chcesz grac dalej wprowadź nową liczbę.");
             else
                 Console.WriteLine($"Wprowadzona przez Ciebie liczba {number} nie spełnia logiki FizzBuzz. Jeśli chcesz grac dalej wprowadź nową liczbę.");
-                
-            GetNumber();
+
+            var output = Console.ReadLine();
+            
+            return output;
         }
     }
 }
--- Tydz4_cw2_FizzBuzz/Program.cs
+++ Tydz4_cw2_FizzBuzz/Program.cs
@@ -1,15 +1,29 @@
-﻿using System.Runtime.CompilerServices;
-
-namespace Tydz4_cw2_FizzBuzz
+﻿namespace Tydz4_cw2_FizzBuzz
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Wprowadź dowolną liczbę całkowitą i zagrajmy w FizzBuzz!");
+            Console.WriteLine("Wprowadź dowolną liczbę całkowitą i zagrajmy w FizzBuzz!\nJeżeli chcesz zakończyć działanie aplikacji wprowadź 'T'");
 
-            FizzBuzz newTry= new FizzBuzz();
-            newTry.GetNumber();
+            var input = Console.ReadLine();
+
+            while (true)
+            {
+                if (input.ToUpper() == "T")
+                {
+                    Environment.Exit(0);
+                }
+                if (!int.TryParse(input, out int number))
+                {
+                    Console.WriteLine("Wprowadzona liczba nie jest liczbą całkowitą lub wykracza poza jej zakres. Podaj nową liczbę całkowitą:");
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                input = FizzBuzz.Message(number);
+            }
+
         }
     }
 }
